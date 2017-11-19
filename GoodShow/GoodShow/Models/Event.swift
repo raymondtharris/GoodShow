6cd05f030a634622e48000a96f3915c7a307c942
Tim Harris <raymondtimothyharris@gmail.com>
2017-11-18 18:04:57 -0800

    Added view controllers to storyboard. created classes for user and event. working on loading data into collection view.

--- GoodShow/GoodShow/Models/Event.swift
+++ GoodShow/GoodShow/Models/Event.swift
@@ -9,25 +9,32 @@
 import Foundation
 import Photos
 
+var photoArray:PHFetchResult<PHAsset> = PHFetchResult()
+
 class goodshowEvent{
     var name: String = ""
 }
 
 
-func loadPhotoLibrary() -> Void {
+func loadPhotoLibrary() -> PHFetchResult<PHAsset> {
     PHPhotoLibrary.requestAuthorization({(status:PHAuthorizationStatus) in
         switch status {
         case .notDetermined:
             print("no determined")
+            //return nil
         case .restricted:
             print("maybe a little")
+            //return nil
         case .denied:
             print("nope")
+            //return nil
         case .authorized:
             print("YES")
-            let options = PHFetchOptions()
-            let photos = PHAsset.fetchAssets(with: .image, options: options)
+            let photos = PHAsset.fetchAssets(with: .image, options: PHFetchOptions())
             print("\(photos.count) found!")
+            photoArray = photos
+            //return photos
         }
     })
+    return photoArray
 }
--- GoodShow/GoodShow/ViewController.swift
+++ GoodShow/GoodShow/ViewController.swift
@@ -7,20 +7,44 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    
+    var goodshowEvents:PHFetchResult<PHAsset> = PHFetchResult()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        loadPhotoLibrary()
+        goodshowEvents = loadPhotoLibrary()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        print("\(goodshowEvents.count)")
+        return goodshowEvents.count
+    }
 
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("\(goodshowEvents.count)")
+        return goodshowEvents.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let goodshowCell = collectionView.dequeueReusableCell(withReuseIdentifier: "goodshowCell", for: indexPath) as! goodshowEventCell
+        goodshowCell.backgroundColor = UIColor.black
+        return goodshowCell
+    }
 }
 
+
+class goodshowEventCell: UICollectionViewCell {
+    
+}
