d65e9ea57e5c7cd689fc643b3de6c74ba17a1a54
Tim Harris <raymondtimothyharris@gmail.com>
2017-11-24 15:06:13 -0800

    Added Text over image and pulled out location. Need to nest location as text on image and add shadow gradient over to highlight the text.

--- GoodShow/GoodShow/ViewController.swift
+++ GoodShow/GoodShow/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Photos
+import CoreLocation
 
 var goodshowEvents:PHFetchResult<PHAsset> = PHFetchResult()
 
@@ -45,7 +46,8 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
         
         // Set Cell Data
         goodshowCell.image.image = goodshowCell.fetchImage(asset: goodshowEvents.object(at: indexPath.row))
-        goodshowCell.mediaCount.text = "4"
+        goodshowCell.mediaCount.text = goodshowCell.fetchLocationString(aLocation: goodshowEvents.object(at: indexPath.row).location)
+        //print("\(aLocation: goodshowEvents.object(at: indexPath.row).location)")
         goodshowCell.dateUpdated.text = goodshowCell.formatDateString(aDate: goodshowEvents.object(at: indexPath.row).creationDate!)
         return goodshowCell
     }
@@ -63,9 +65,9 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
                 print("nope")
             //return nil
             case .authorized:
-                print("YES")
+                //print("YES")
                 goodshowEvents = PHAsset.fetchAssets(with: .image, options: PHFetchOptions())
-                print("\(goodshowEvents.count) found!")
+                //print("\(goodshowEvents.count) found!")
                 OperationQueue.main.addOperation {
                     self.goodshowCollectionView.reloadData()
                 }
@@ -85,6 +87,8 @@ class goodshowEventCell: UICollectionViewCell {
     @IBOutlet weak var dateUpdated: UILabel!
     @IBOutlet weak var mediaCount: UILabel!
     
+    var imageText:String = ""
+    
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
@@ -92,7 +96,9 @@ class goodshowEventCell: UICollectionViewCell {
         self.layer.borderWidth = 1.0
         self.layer.borderColor = UIColor.clear.cgColor
         self.layer.masksToBounds = true
-
+        
+        self.dateUpdated.textColor = UIColor.gray
+        self.mediaCount.textColor = UIColor.gray
         
     }
     
@@ -111,7 +117,48 @@ class goodshowEventCell: UICollectionViewCell {
         imageManager.requestImage(for: asset, targetSize: CGSize(width: self.image.bounds.width, height: self.image.bounds.height), contentMode: .aspectFit, options: requestOptions, resultHandler: { (result, info) -> Void in
             image = result!
         })
-        return image
+        
+        let locationString = fetchLocationString(aLocation: asset.location)
+        let updatedImage = addLocationToImage(image: image, locationString: locationString, imageView: self.image)
+        
+        return updatedImage
         
     }
+    
+    
+    func fetchLocationString(aLocation:CLLocation?) -> String {
+        var locationString = "test"
+        let geoCoder = CLGeocoder()
+        
+        if let goodLocation = aLocation {
+            geoCoder.reverseGeocodeLocation(goodLocation, completionHandler: {(response, error) in
+                if let addr = response?.first {
+                    //print(addr.locality!)
+                    locationString = addr.locality!
+                    
+                }
+            })
+        }
+        
+        return locationString
+    }
+    
+    func addLocationToImage(image:UIImage, locationString: String, imageView: UIImageView) -> UIImage {
+        let textColor = UIColor.white
+        let textFont = UIFont(name: "Helvetica Bold", size: 12)!
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+        let textFontAttributes = [NSAttributedStringKey.font: textFont, NSAttributedStringKey.foregroundColor: textColor,] as [NSAttributedStringKey : Any]
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        let rect = CGRect(x: 10, y: imageView.bounds.height , width: image.size.width, height: image.size.height)
+        locationString.draw(in: rect, withAttributes: textFontAttributes)
+        
+        let newImageWithText = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImageWithText!
+    }
+    
 }
